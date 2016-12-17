b6a2ff049794d128f80e072ed197bc7b017c2769
name <604076950@qq.com>
2016-12-17 23:38:02 +0800

    submit TDD

---
+++ chenmengting/myTab/src/howrk/f14012206/chen/test/TableDefineParser.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package howrk.f14012206.chen.test;
+
+/**
+ * @author chenmengting
+ *
+ */
+public class TableDefineParser {
+
+	public Table parse(String tableDefineStr) {
+		// TODO Auto-generated method stub
+		return new Table();
+	}
+
+}
